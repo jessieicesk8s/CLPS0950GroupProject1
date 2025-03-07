464e4bac9dbb8f76f3373f5635e18dc32a0c3495
jessieicesk8s <jessica_m_lee@brown.edu>
2025-03-07 18:24:19 -0500

    Started Matrix 1

---
+++ Matrix1.m
@@ -0,0 +1,16 @@
+matrix_1a = zeros(10, 10);
+target = 'X';
+%target
+disp(matrix_1a)
+
+matrix_1b = zeros(10, 10);
+%disp(matrix_1b)
+
+matrix_1c = zeros(10, 10);
+%disp(matrix_1c)
+
+matrix_1d = zeros(10, 10);
+%disp(matrix_1d)
+
+matrix_1e = zeros(10, 10);
+%disp(matrix_1e)
