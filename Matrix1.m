be3ec500eaee5806af67b6ce6d240f0db1ba250f
jessieicesk8s <jessica_m_lee@brown.edu>
2025-03-14 13:00:37 -0400

    Fixed bugs, adjusted figure window, and fixed means
    Added instructions to the first figure window, and made it so mean is now accurate for both elapsed time and accuracy!!

--- Matrix1.m
+++ Matrix1.m
@@ -1,12 +1,19 @@
-% Participant Instructions
-disp('Press the key that corresponds to the quadrant that contains the target ("X")')
-disp('Q = Top Left, W = Top Right, A = Bottom Left, S = Bottom Right')
-disp('Press any key to start the experiment.')
-pause; % Wait for participant to be ready; any key will make code continue running
+%Participant Instructions
+fig = figure('Name', 'Stimulus Search', 'NumberTitle', 'off', 'MenuBar', 'none', 'ToolBar', 'none');
+instructions = sprintf(['Press the key that corresponds to the quadrant that contains the target ("X")\n\n' ...
+    'Q = Top Left, W = Top Right, A = Bottom Left, S = Bottom Right\n\n' ...
+    'Press any key to start the experiment.']);
+text(0.5, 0.5, instructions, 'FontSize', 15, 'HorizontalAlignment', 'center', 'VerticalAlignment', 'middle');
+axis off;
+disp('Instructions displayed in separate window. Press any key to start.')
+pause;
+
+clf(fig); %Clear the figure window
+text(0.5, 0.5, matrix_1a, 'FontSize', 15, 'HorizontalAlignment', 'center', 'VerticalAlignment', 'middle');
+axis off;
 
 %Below are Matrices 1a-1e
 %Matrix 1a, Target Stimulus in Upper Left Quadrant
-%f = figure;
 matrix_1a = repmat('0', 10, 20); %Base 10x20 matrix of zeros
 matrix_1a(2, 4) = 'X'; %Placing target stimulus in UL Quadrant
 disp(matrix_1a)
@@ -14,14 +21,14 @@ disp(matrix_1a)
 tic; %Start timer for Matrix 1a Task
 while toc < 10
     if waitforbuttonpress
-        elapsed_time = toc;
+        elapsed_time_a = toc;
         key = get(gcf, 'CurrentCharacter');
         if key == 'q'
-            is_correct = true;
-            %true = 1;
+            is_correct_a = true;
+            true = 1;
         else
-            is_correct = false;
-            %false = 0;
+            is_correct_a = false;
+            false = 0;
         end
         break;
     end
@@ -29,6 +36,10 @@ end
 
 pause(0.5);
 clc;
+
+clf(fig);
+text(0.5, 0.5, matrix_1b, 'FontSize', 15, 'HorizontalAlignment', 'center', 'VerticalAlignment', 'middle');
+axis off; 
 
 %Matrix 1b, Target Stimulus in Bottom Right Quadrant
 matrix_1b = repmat('0', 10, 20); %Base 10x20 matrix of zeros
@@ -38,12 +49,14 @@ disp(matrix_1b)
 tic; %Start timer for Matrix 1b Task
 while toc < 10
     if waitforbuttonpress
-        elapsed_time = toc;
+        elapsed_time_b = toc;
         key = get(gcf, 'CurrentCharacter');
         if key == 's'
-            is_correct = true;
+            is_correct_b = true;
+            true = 1;
         else
-            is_correct = false;
+            is_correct_b = false;
+            false = 0;
         end
         break;
     end
@@ -51,6 +64,10 @@ end
 
 pause(0.5);
 clc;
+
+clf(fig);
+text(0.5, 0.5, matrix_1c, 'FontSize', 15, 'HorizontalAlignment', 'center', 'VerticalAlignment', 'middle');
+axis off; 
 
 %Matrix 1c, Target Stimulus in Upper Right Quadrant
 matrix_1c = repmat('0', 10, 20); %Base 10x20 matrix of zeros
@@ -60,12 +77,14 @@ disp(matrix_1c)
 tic; %Start timer for Matrix 1c Task
 while toc < 10
     if waitforbuttonpress
-        elapsed_time = toc;
+        elapsed_time_c = toc;
         key = get(gcf, 'CurrentCharacter');
         if key == 'w'
-            is_correct = true;
+            is_correct_c = true;
+            true = 1;
         else
-            is_correct = false;
+            is_correct_c = false;
+            false = 0;
         end
         break;
     end
@@ -73,6 +92,10 @@ end
 
 pause(0.5);
 clc;
+
+clf(fig);
+text(0.5, 0.5, matrix_1d, 'FontSize', 15, 'HorizontalAlignment', 'center', 'VerticalAlignment', 'middle');
+axis off; 
 
 %Matrix 1d, Target Stimulus in Bottom Left Quadrant
 matrix_1d =  repmat('0', 10, 20); %Base 10x20 matrix of zeros
@@ -82,12 +105,14 @@ disp(matrix_1d)
 tic; %Start timer for Matrix 1d Task
 while toc < 10
     if waitforbuttonpress
-        elapsed_time = toc;
+        elapsed_time_d = toc;
         key = get(gcf, 'CurrentCharacter');
         if key == 'a'
-            is_correct = true;
+            is_correct_d = true;
+            true = 1;
         else
-            is_correct = false;
+            is_correct_d = false;
+            false = 0;
         end
         break;
     end
@@ -95,6 +120,10 @@ end
 
 pause(0.5);
 clc;
+
+clf(fig);
+text(0.5, 0.5, matrix_1e, 'FontSize', 15, 'HorizontalAlignment', 'center', 'VerticalAlignment', 'middle');
+axis off; 
 
 %Matrix 1e, Target Stimulus in Bottom Right Quadrant
 matrix_1e =  repmat('0', 10, 20); %Base 10x20 matrix of zeros
@@ -104,12 +133,14 @@ disp(matrix_1e);
 tic; %Start timer for Matrix 1e Task
 while toc < 10
     if waitforbuttonpress
-        elapsed_time = toc;
+        elapsed_time_e = toc;
         key = get(gcf, 'CurrentCharacter');
         if key == 's'
-            is_correct = true;
+            is_correct_e = true;
+            true = 1;
         else
-            is_correct = false;
+            is_correct_e = false;
+            false = 0;
         end
         break;
     end
@@ -117,10 +148,14 @@ end
 
 pause(0.5);
 clc;
+clf(fig);
 
-mean_reactiontime = mean(elapsed_time);
-overall_accuracy = mean(num2str(is_correct)) * 100;
+ET = ([elapsed_time_a, elapsed_time_b, elapsed_time_c, elapsed_time_d, elapsed_time_e]);
+IC = ([is_correct_a, is_correct_b, is_correct_c, is_correct_d, is_correct_e]);
 
-disp('Results');
-disp(strcat('Mean Reaction Time:', num2str(mean_reactiontime)))
+mean_reactiontime = mean(ET);
+overall_accuracy = mean(IC) * 100;
+
+disp('Results:');
+disp(strcat('Mean Reaction Time:', num2str(mean_reactiontime), ' seconds'))
 disp(strcat('Overall Accuracy: ', num2str(overall_accuracy), '%'))
